62f40eec1b2ee1506b6ac0edb81573e5f1c64e25
tetsuo-kouyama <kouyamatetuo1129@gmail.com>
2026-08-15 17:45:04 +0900

    chore: app:updateによるファイル変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_08_09_153433) do
+ActiveRecord::Schema[8.0].define(version: 2026_08_15_080915) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension "pg_catalog.plpgsql"
 
   create_table "adventure_events", force: :cascade do |t|
     t.bigint "adventure_id", null: false
